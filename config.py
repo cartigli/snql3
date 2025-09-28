85f0940039246dc8f5a31dd96b1f0d38dae8cf43
cartigli <tommy@cartiglia.co>
2025-09-28 17:05:10 -0500

    download script's ability to pull and rebuilt the folder structure & note contents is implemeented/tested

--- config.py
+++ config.py
@@ -3,8 +3,8 @@ import os
 DB_USER = os.getenv('DB_USER','notation')
 PASS_PHRASE = os.getenv('PASS_PHRASE','0n3*4t!2D*62')
 DATABASE_NAME = os.getenv('DATABASE_NAME','notes')
-TABLE_NAME = os.getenv('TABLE_NAME','notes')
+TABLE_NAME = os.getenv('TABLE_NAME','notes_2')
 DATABASE_ADDR = os.getenv('DATABASE_ADDR','192.168.1.68')
 HOST_PORT = os.getenv('HOST_PORT','3306')
 
-LOCAL_DIR = os.getenv('LOCAL_DIR','/Volumes/HomeXx/compuir/vault')
+LOCAL_DIR = os.getenv('LOCAL_DIR','/Volumes/HomeXx/compuir/Desktop/vault_test')
--- download.py
+++ download.py
@@ -37,3 +37,31 @@ def write_to_disk(notes):
 		os.makedirs(directory, exist_ok=True)
 		with open(path, 'w') as f:
 			f.write(contents)
+
+# copied from upload
+def initialize_connection(DB_USER, PASS_PHRASE, DATABASE_NAME, DATABASE_ADDR):
+	try:
+		conn = mysql.connector.connect(
+			host=DATABASE_ADDR,
+			user=DB_USER,
+			password=PASS_PHRASE,
+			database=DATABASE_NAME
+		)
+		return conn
+	except Exception as e:
+		print(f"ERR {e}: CHECK CONFIG")
+		return None
+
+if __name__ == "__main__":
+	conn = initialize_connection(DB_USER, PASS_PHRASE, DATABASE_NAME, DATABASE_ADDR)
+	if conn:
+		try:
+			notes_remote = survey_db(conn, TABLE_NAME)
+			guts = layout_guts(notes_remote)
+			glory = contain(*guts)
+			write_to_disk(glory)
+		finally:
+			conn.close()
+			print(f"Got the files to the local_machine, boss!")
+	else:
+		print(f"Something went wrong, boss. Check the connection & config, please.")
