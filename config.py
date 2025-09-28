9015d6ebe47bef033f82ec6acb4a4d63ce5915c4
cartigli <tommy@cartiglia.co>
2025-09-28 16:44:43 -0500

    initial upload script & config file for base-level starting point

---
+++ config.py
@@ -0,0 +1,10 @@
+import os
+
+DB_USER = os.getenv('DB_USER','notation')
+PASS_PHRASE = os.getenv('PASS_PHRASE','0n3*4t!2D*62')
+DATABASE_NAME = os.getenv('DATABASE_NAME','notes')
+TABLE_NAME = os.getenv('TABLE_NAME','notes')
+DATABASE_ADDR = os.getenv('DATABASE_ADDR','192.168.1.68')
+HOST_PORT = os.getenv('HOST_PORT','3306')
+
+LOCAL_DIR = os.getenv('LOCAL_DIR','/Volumes/HomeXx/compuir/vault')
